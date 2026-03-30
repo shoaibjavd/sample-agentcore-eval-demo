6a62fd06b182af91600354cd470633e341c0fa35
Mahsa Paknezhad <mahsap@amazon.com>
2026-03-30 11:27:37 +1100

    fix bug

--- mcp-server/src/auth/__init__.py
+++ mcp-server/src/auth/__init__.py
@@ -1,4 +1,3 @@
-from src.auth.models import AccessToken
-from src.auth.utils import auth_meta, get_access_token, parse_jwt_claims, set_access_token
+from src.auth.utils import auth_meta, get_access_token
 
-__all__ = ["AccessToken", "auth_meta", "get_access_token", "set_access_token", "parse_jwt_claims"]
+__all__ = ["auth_meta", "get_access_token"]
